ddf1c85724beadec52da077983c3673e21a8bf1e
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-11-29 22:07:12 +0900

    add comment

--- setup.sh
+++ setup.sh
@@ -15,29 +15,36 @@ SERVICE_ACCOUNT_NAME="terraform-runner"
 SERVICE_ACCOUNT_KEY_FILE_NAME="./terraform/"${SERVICE_ACCOUNT_NAME}-key.json
 GCS_BUCKET_NAME=gs://${PROJECT_ID}-tfstate
 
-
+# login to GCP
 gcloud auth login
-gcloud projects create ${PROJECT_ID} --name=${PROJECT_NAME}
 
-gcloud beta billing accounts list
+# create new project
+gcloud projects create ${PROJECT_ID} --name=${PROJECT_NAME}
 gcloud beta billing projects link ${PROJECT_ID} --billing-account=${BILLING_ACCOUNT}
 
+# api enablee
 gcloud services enable run.googleapis.com --project=${PROJECT_ID}
 gcloud services enable iam.googleapis.com --project=${PROJECT_ID}
 gcloud services enable cloudscheduler.googleapis.com --project=${PROJECT_ID}
 
+# create service account
 gcloud iam service-accounts create ${SERVICE_ACCOUNT_NAME} --display-name "${SERVICE_ACCOUNT_NAME}" --project ${PROJECT_ID}
+
+# add policy
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/run.admin"
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/storage.admin"
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/iam.serviceAccountUser"
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/artifactregistry.admin"
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/cloudscheduler.admin"
 
+# create key file
 gcloud iam service-accounts keys create ${SERVICE_ACCOUNT_KEY_FILE_NAME} --iam-account ${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
-gcloud storage buckets create ${GCS_BUCKET_NAME} --project ${PROJECT_ID} --location ${GCS_REGION} --uniform-bucket-level-access
 
+# setup GCS bucket
+gcloud storage buckets create ${GCS_BUCKET_NAME} --project ${PROJECT_ID} --location ${GCS_REGION} --uniform-bucket-level-access
 gcloud storage buckets add-iam-policy-binding ${GCS_BUCKET_NAME} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/storage.admin"
 
+# display finish
 echo -e "\033[32m===================================================\033[0m"
 echo -e "\033[32mfinish to create GCP resources!\033[0m"
 echo ""
