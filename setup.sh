f63046c484f8d43e0c2d154792a3c3e13b99d9e8
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-11-29 15:22:44 +0900

    change keyfile filepath

--- setup.sh
+++ setup.sh
@@ -12,12 +12,35 @@ read -p "? billing account ID: " BILLING_ACCOUNT
 read -p "? GCS region: " GCS_REGION
 
 SERVICE_ACCOUNT_NAME="terraform-runner"
-SERVICE_ACCOUNT_KEY_FILE_NAME=${SERVICE_ACCOUNT_NAME}-key.json
+SERVICE_ACCOUNT_KEY_FILE_NAME="./terraform/"${SERVICE_ACCOUNT_NAME}-key.json
 GCS_BUCKET_NAME=gs://${PROJECT_ID}-tfstate
 
+
+gcloud auth login
+gcloud projects create ${PROJECT_ID} --name=${PROJECT_NAME}
+
+gcloud beta billing accounts list
+gcloud beta billing projects link ${PROJECT_ID} --billing-account=${BILLING_ACCOUNT}
+
+gcloud services enable run.googleapis.com --project=${PROJECT_ID}
+gcloud services enable iam.googleapis.com --project=${PROJECT_ID}
+
+gcloud iam service-accounts create ${SERVICE_ACCOUNT_NAME} --display-name "${SERVICE_ACCOUNT_NAME}" --project ${PROJECT_ID}
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/run.admin"
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/storage.admin"
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/iam.serviceAccountUser"
+
+gcloud iam service-accounts keys create ${SERVICE_ACCOUNT_KEY_FILE_NAME} --iam-account ${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
+gcloud storage buckets create ${GCS_BUCKET_NAME} --project ${PROJECT_ID} --location ${GCS_REGION} --uniform-bucket-level-access
+
+gcloud storage buckets add-iam-policy-binding ${GCS_BUCKET_NAME} --member "serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/storage.admin"
 
 echo -e "\033[32m===================================================\033[0m"
 echo -e "\033[32mfinish to create GCP resources!\033[0m"
 echo ""
 echo -e "\033[32mservice account secret: ./${SERVICE_ACCOUNT_KEY_FILE_NAME}\033[0m"
+echo -e "next:"
+echo -e "cd terraform"
+echo -e "terraform plan"
+echo -e "terraform apply"
 echo -e "\033[32m===================================================\033[0m"
